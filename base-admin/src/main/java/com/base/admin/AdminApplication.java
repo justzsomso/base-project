627db410d0127ae0d1ce8cacf5ddac3cc4333fb7
Zhou Min <132646373+justzsomso@users.noreply.github.com>
2025-11-03 21:38:22 +0800

    基础功能部分搭建

--- base-admin/src/main/java/com/base/admin/AdminApplication.java
+++ base-admin/src/main/java/com/base/admin/AdminApplication.java
@@ -1,13 +1,9 @@
 package com.base.admin;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.base") // 扫描所有模块的组件
-@MapperScan("com.base.*.mapper") // 扫描所有模块的Mapper接口
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
